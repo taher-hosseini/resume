28d0c6cc6e84234044c8a93e69f0271019d3407f
Taher <taherhossieninasab@gmail.com>
2024-08-05 00:22:30 +0330

    home section responsive

--- components/AboutUs.js
+++ components/AboutUs.js
@@ -10,11 +10,11 @@ export default function AboutUs() {
                     <p className="section-head__caption"></p>
                     <span className="section-head__line"></span>
                 </div>
-                <div className={`${styles.about} about_g`}>
-                    <div className={`${styles.about__cover}`}>
+                <div className={`${styles.about} row about_g`}>
+                    <div className={`col-12 col-lg-6 ${styles.about__cover}`}>
                         <img className={styles.about__img} src="/images/taher2.JPG" alt="about"/>
                     </div>
-                    <div className={`${styles.about__content}`}>
+                    <div className={`col-12 col-lg-6 ${styles.about__content}`}>
                         <h4 className={styles.about__title}>معرفی:</h4>
                         <p className={styles.about__description}>
                             سلام! من طاهر حسینی نسب، فارغ التحصیل کارشناسی ارشد مهندسی نرم افزار از دانشگاه مدنی آذربایجان هستم. به عنوان توسعه دهنده فرانت وب با تخصص در React فعالیت می کنم. هدف من ایجاد رابط های کاربری دینامیک و بهینه سازی عملکرد وب سایت ها است. به دنبال فرصت های همکاری در پروژه های جدید هستم تا بهترین تجربه های کاربری را ارائه دهم.
